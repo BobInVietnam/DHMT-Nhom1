601fab07445e356777da83272bd74e2b2d258035
DarlingSolobymyself <your-email@example.com>
2026-04-23 09:27:44 +0700

    Merge code

--- js/character.js
+++ js/character.js
@@ -1,34 +1,29 @@
-class Character {
-  constructor() {
-    this.isVisible = false;
-    this.x = 0;
-    this.y = 0;
-    // đổi link ảnh 
-    // this.img = loadImage('assets/cogiao.png'); 
-  }
+class Character extends Entity {
+    constructor() {
+        super(0, 0, 150, 300);
+        this.isVisible = false;
+        //load ảnh
+    }
 
-  show(x, y) {
-    this.x = x;
-    this.y = y;
-    this.isVisible = true;
-  }
+    show(posX, posY) {
+        this.x = posX;
+        this.y = posY;
+        this.isVisible = true;
+    }
 
-  hide() {
-    this.isVisible = false;
-  }
+    hide() {
+        this.isVisible = false;
+    }
 
-  draw() {
-    if (!this.isVisible) return;
-    
-    push();
-    // Nếu có ảnh thì dùng: image(this.img, this.x, this.y, 100, 200);
-    
-    // giả lập nhân vật
-    fill(200, 220, 255);
-    rect(this.x, this.y, 80, 180, 10); 
-    fill(0);
-    textAlign(CENTER, CENTER);
-    text("Cô\ngiáo", this.x + 40, this.y + 90);
-    pop();
-  }
+    display() {
+        if (!this.isVisible) return;
+        
+        push();
+        fill(200, 220, 255);
+        rect(this.x, this.y, this.w, this.h, 10); 
+        fill(0);
+        textAlign(CENTER, CENTER);
+        text("Cô\ngiáo", this.x + this.w / 2, this.y + this.h / 2);
+        pop();
+    }
 }
