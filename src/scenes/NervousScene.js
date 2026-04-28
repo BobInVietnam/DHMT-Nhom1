3e587d2f6b8a931b6f2bda33dfbdee0da6131cb5
BobInVietnam <xuanhungcao204@gmail.com>
2026-04-28 16:35:44 +0700

    Revert "Hoan thanh NervousScene: them giao vien va hinh anh bo nao tu assets"
    This reverts commit ed2e78f3b3e53ae8dd4160784bfc11d51e2aa110.

--- src/scenes/NervousScene.js
+++ src/scenes/NervousScene.js
@@ -1,47 +1,15 @@
 class NervousScene extends Scene {
     constructor() {
         super();
-        
-        // 1. Nút Back để quay lại bản đồ cơ thể (BodyMap)
         this.backBtn = new Button(100, 50, 150, 40, "Back", "SWITCH_SCENE", "BodyMap");
         this.objects.push(this.backBtn);
-
-        // 2. Thêm Hình ảnh Hệ Thần Kinh (Dữ liệu từ assets.json: "brain")
-        // Đặt ở vị trí trung tâm bên trái như trong Figma
-        this.nervousDisplay = new ImageEntity(450, 400, 500, 500, "brain", true);
-        this.objects.push(this.nervousDisplay);
-
-        // 3. Thêm Giáo viên (Dữ liệu từ assets.json: "teach_point_talk")
-        // Đặt ở bên phải, đang chỉ tay vào nội dung
-        this.teacher = new ImageEntity(1000, 500, 400, 600, "teach_point_talk", true);
-        this.objects.push(this.teacher);
-
-        // 4. Ví dụ thêm một ImageButton để tương tác (Ví dụ: Nút Xem chi tiết Neuron)
-        // Sử dụng một asset có sẵn làm nút, giả sử dùng "teach_normal" làm placeholder
-        this.detailBtn = new ImageButton(1000, 700, 200, 80, "teach_normal", true, "SHOW_DETAIL", "neuron_info");
-        this.objects.push(this.detailBtn);
     }
 
     draw() {
-        // Màu nền xám trung tính giống trong Figma
-        background(220); 
-        
-        // Vẽ tiêu đề bài học
-        push();
-        fill(0);
-        textSize(32);
+        background(180);
         textAlign(CENTER);
-        text("HỆ THẦN KINH - NERVOUS SYSTEM", width / 2, 60);
-        
-        // Vẽ bong bóng thoại giả định cho giáo viên (như trong Figma)
-        fill(255);
-        rect(800, 150, 300, 100, 20); // Khung lời thoại
-        fill(0);
-        textSize(18);
-        text("Đây là bộ não, cơ quan\nđiều khiển mọi hoạt động!", 950, 195);
-        pop();
+        text("Nervous System", width / 2, 100);
 
-        // Gọi hàm draw của lớp cha để vẽ tất cả objects (Entity, Button, ImageEntity...)
         super.draw();
     }
 }
